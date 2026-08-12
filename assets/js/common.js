8d7d396049aa5c7fd79258207100974ece3d50a6
마정운 <mjw0775@naver.com>
2026-08-12 18:28:18 +0900

    최종

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,8 +1,27 @@
 'use strict';
 
+// header관련
 const header = document.querySelector('#header')
 const btnOpen = document.querySelector('.btn-open');
-
 btnOpen.addEventListener('click', () => {
     header.classList.toggle('show');
+})
+
+// intersectionobserver
+const section = document.querySelectorAll('section');
+const sectionOptions = {
+    rootMargin: '-30%',
+    threshold: [0,1]
+}
+const sectionObserver = new IntersectionObserver(sectionEffect,sectionOptions);
+function sectionEffect(entries) {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('active')
+        }
+    })
+}
+
+section.forEach((e) => {
+    sectionObserver.observe(e)
 })
