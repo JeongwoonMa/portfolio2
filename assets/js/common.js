d25e33f05874235230e11145af6aef46ba7c134a
마정운 <mjw0775@naver.com>
2026-08-12 17:17:53 +0900

    20260812

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const header = document.querySelector('#header')
+const btnOpen = document.querySelector('.btn-open');
+
+btnOpen.addEventListener('click', () => {
+    header.classList.toggle('show');
+})
