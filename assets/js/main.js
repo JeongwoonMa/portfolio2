7a67eef3f2c41d7ce2fc6e877e01fd0d15691c9d
마정운 <mjw0775@naver.com>
2026-08-19 17:39:43 +0900

    8/18

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,67 @@
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, SplitText);
 
-let splitTxt = new SplitText('.split-title', {type: 'chars, words, lines'});
+let splitTxt = new SplitText(".split-title", { type: "chars, words, lines" });
 let titleSplit = splitTxt.chars;
 
-gsap.to('.bg-list .bg-img img', {
+gsap.to(".bg-list .bg-img img", {
     opacity: 0.3,
     duration: 1,
     repeat: -1,
     yoyo: true,
-    ease: "power1.inOut"
-})
+    ease: "power1.inOut",
+});
 
 const tl_1 = gsap.timeline({
     scrollTrigger: {
-        trigger: '#about',
-        start: 'top 1%',
-        end: 'bottom bottom',
-    }
-})
-
-tl_1.from('.bg-list .bg-img', {
-    backgroundColor: '#000000',
+        trigger: "#about",
+        start: "top 1%",
+        end: "bottom bottom",
+    },
+});
+tl_1.from(".bg-list .bg-img", {
+    backgroundColor: "#000000",
     opacity: 0,
     duration: 3,
-    
 })
-.from('.bg-list .bg-moon', {
-    y: '-2%',
-    opacity: 0
-},'-=2')
-.from('.bg-list .bg-earth', {
-    y: '10%',
-    duration: 1.5,
-    opacity: 0
-},'-=1.9')
-.from(titleSplit, {
-    opacity: 0,
-    stagger: {
-        amount: 0.1,
-        from: 'random'
-    },
-    ease: 'back.out',
-    repeat: -1,
-})
-
-const tl_end = gsap.timeline({
+    .from(
+        ".bg-list .bg-moon",
+        {
+            y: "-2%",
+            opacity: 0,
+        },
+        "-=2",
+    )
+    .from(
+        ".bg-list .bg-earth",
+        {
+            y: "10%",
+            duration: 1.5,
+            opacity: 0,
+        },
+        "-=1.9",
+    )
+    .from(titleSplit, {
+        y: "50%",
+        opacity: 0,
+        stagger: 0.2,
+        ease: "back.out",
+    });
+const tl_2 = gsap.timeline({
     scrollTrigger: {
-        trigger: ''
-    }
+        trigger: "#about",
+        start: "bottom 100%", // #about 끝날 때 시작
+        end: "+=700",
+        scrub: true,
+        pin: true,
+    },
+});
+
+tl_2.to("#about .split-title", {
+    scale: 50,
+    transformOrigin: "center center",
+    duration: 5
 })
-
-
+.to(".bg-list .bg-fade", {
+    backgroundColor: "#000",
+    duration: 5
+},0);
