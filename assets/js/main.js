96dcd31a16136b160340d5e9a6dbbcd15cd73732
마정운 <mjw0775@naver.com>
2026-08-20 14:05:02 +0900

    8/20

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,69 +1,74 @@
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, SplitText);
 
-let splitTxt = new SplitText(".split-title", { type: "chars, words, lines" });
-let titleSplit = splitTxt.chars;
+window.onload = () => {
+    let splitTxt = new SplitText(".split-title", { type: "chars, words, lines" });
+    let titleSplit = splitTxt.chars;
 
-gsap.to(".bg-list .bg-img img", {
-    opacity: 0.3,
-    duration: 1,
-    repeat: -1,
-    yoyo: true,
-    ease: "power1.inOut",
-});
+    gsap.to(".bg-list .bg-img img", {
+        opacity: 0.3,
+        duration: 1,
+        repeat: -1,
+        yoyo: true,
+        ease: "power1.inOut",
+    });
 
-const tl_1 = gsap.timeline({
-    scrollTrigger: {
-        trigger: "#intro",
-        start: "top 1%",
-        end: "bottom bottom",
-    },
-});
-tl_1.from(".bg-list .bg-img", {
-    backgroundColor: "#000000",
-    opacity: 0,
-    duration: 3,
-})
-    .from(
-        ".bg-list .bg-moon",
-        {
-            y: "-2%",
-            opacity: 0,
+    const tl_1 = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#intro",
+            start: "top 1%",
+            end: "bottom bottom",
         },
-        "-=2",
-    )
-    .from(
-        ".bg-list .bg-earth",
-        {
-            y: "10%",
-            duration: 1.5,
-            opacity: 0,
-        },
-        "-=1.9",
-    )
-    .from(titleSplit, {
-        y: "50%",
+    });
+    tl_1.from(".bg-list .bg-img", {
+        backgroundColor: "#000000",
         opacity: 0,
-        stagger: 0.2,
-        ease: "back.out",
+        duration: 3,
     })
-const tl_2 = gsap.timeline({
-    scrollTrigger: {
-        trigger: "#intro",
-        start: "bottom 100%", // #intro 끝날 때 시작
-        end: "+=1500",
-        scrub: true,
-        pin: true,
-    },
-});
+        .from(
+            ".bg-list .bg-moon",
+            {
+                y: "-2%",
+                opacity: 0,
+            },
+            "-=2",
+        )
+        .from(
+            ".bg-list .bg-earth",
+            {
+                y: "10%",
+                duration: 1.5,
+                opacity: 0,
+            },
+            "-=1.9",
+        )
+        .from(titleSplit, {
+            y: "50%",
+            opacity: 0,
+            stagger: 0.2,
+            ease: "back.out",
+        });
+    const tl_2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#intro",
+            start: "bottom 100%", // #intro 끝날 때 시작
+            end: "+=1500",
+            scrub: true,
+            pin: true,
+        },
+    });
 
-tl_2.to("#intro .split-title", {
-    scale: 50,
-    transformOrigin: "center center",
-    opacity: 0,
-    duration: 5,
-    className: 'split-title'
-})
-.to(".bg-list .bg-fade", {
-    backgroundColor: "#000",
-    duration: 5
-},0);
+    tl_2.to("#intro .split-title", {
+        scale: 50,
+        transformOrigin: "center center",
+        opacity: 0,
+        duration: 5,
+        className: "split-title",
+    }).to(
+        ".bg-list .bg-fade",
+        {
+            backgroundColor: "#000",
+            duration: 5,
+        },
+        0,
+    );
+};
